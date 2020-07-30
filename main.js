6dea0f2a422505ca108995adf37df9508ace4e1e
Matt Carreon <mattcarryon@gmail.com>
2020-07-29 22:20:57 -0700

    hovering functionality

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 for (let i=0; i<16;i++) {
     let square = document.createElement('div');
     square.className = "square";
-
+    square.addEventListener("mouseover", fill);
     grid = document.getElementsByClassName("grid")[0];
     grid.appendChild(square);
 };
+
+function fill(){
+    this.classList.add("filled")
+}
