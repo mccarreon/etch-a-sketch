5bca3adb0b964206ca93f4d1ddc41a75b33a4401
Matt Carreon <mattcarryon@gmail.com>
2020-07-30 17:48:57 -0700

    reset button

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const reset = document.getElementById("reset");
+
+reset.addEventListener("click", clear_board);
+
 for (let i=0; i<16;i++) {
     let square = document.createElement('div');
     square.className = "square";
@@ -8,4 +12,11 @@ for (let i=0; i<16;i++) {
 
 function fill(){
     this.classList.add("filled")
-}
+};
+
+function clear_board(){
+    let elements = document.getElementsByClassName('square');
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('filled');
+    }
+};
